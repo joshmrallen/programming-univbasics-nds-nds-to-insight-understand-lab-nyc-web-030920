243f0ce1b7fc527cfc3bcbde7554ecae09fcb55c
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-26 03:48:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -17,7 +17,7 @@ def print_first_directors_movie_titles
   index = 0
   movie_index = 0
   while movie_index < directors_database[0][:movies].count do
-    puts "directors_database[0][:movies][movie_index][:title]"
+    puts "#{directors_database[0][:movies][movie_index][:title]}\n"
     movie_index += 1
   end
   
