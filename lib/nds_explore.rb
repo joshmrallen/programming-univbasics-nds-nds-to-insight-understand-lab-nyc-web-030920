eeef84b93d8060cb7db649c1f27723f9974be14f
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-26 03:30:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
-directors_database
+p directors_database
 
 
 def pretty_print_nds(nds)
@@ -10,4 +10,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  
 end
